6c874b66ed66027f91c52718021511b2c681b597
Max Kryukov <mozt@mozt.org>
2024-03-04 17:58:27 +0300

    etcd-change-size-counter

---
+++ etcd-change-size-counter/README.md
@@ -0,0 +1,2 @@
+Collect changes to file with `etcdctl watch --prefix /registry` during some period.
+Run script after it
---
+++ etcd-change-size-counter/main.py
@@ -0,0 +1,28 @@
+import json
+
+res = {}
+with open('etcd-changes.txt', 'rb') as f:
+    lines = f.readlines()
+    key = ''
+    size = 0
+    marker = False
+    for l in lines:
+        if marker:
+            key_long = l.decode('utf-8').strip()
+            key_long = key_long.split('/', 4)
+            key = '/'.join(key_long[:-1]
+                           ) if len(key_long) > 4 else '/'.join(key_long)
+            marker = False
+        elif l == bytes([0x50, 0x55, 0x54, 0x0D, 0x0A]):
+            if key:
+                # if key in res:
+                #     size += res[key]
+                res[key] = res[key] + size if key in res else size
+                key = ''
+            marker = True
+            size = 0
+        else:
+            size += len(l)
+
+
+print(json.dumps(res))
